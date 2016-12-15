6350d8fb5838e6e02e53ba0e00f0573aa6ac680f
Rangerkriby <rangerkriby@hotmail.com>
2016-12-16 01:30:49 +0800

    增加Hystrix断路器机制
    增加Hystrix断路器机制

--- spring-cloud-feign/src/main/java/com/rkg/consume/client/ComputeClient.java
+++ spring-cloud-feign/src/main/java/com/rkg/consume/client/ComputeClient.java
@@ -5,8 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("biz-service")
+@FeignClient(value = "compute-service", fallback = BizClientHystrix.class)
 public interface ComputeClient {
-    @RequestMapping(method = RequestMethod.GET, value = "/add")
-    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+	@RequestMapping(method = RequestMethod.GET, value = "/add")
+	Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
 }
--- spring-cloud-ribbon/src/main/java/com/rkg/consumer/controller/ConsumerController.java
+++ spring-cloud-ribbon/src/main/java/com/rkg/consumer/controller/ConsumerController.java
@@ -4,14 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
+
+import com.rkg.consumer.service.BizService;
 
 @RestController
 public class ConsumerController {
-    @Autowired
-    RestTemplate restTemplate;
-    @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add() {
-        return restTemplate.getForEntity("http://BIZ-SERVICE/add?a=10&b=20", String.class).getBody();
-    }
+	@Autowired
+	BizService bizService;
+
+	@RequestMapping(value = "/add", method = RequestMethod.GET)
+	public String add() {
+		return bizService.addService();
+	}
 }
