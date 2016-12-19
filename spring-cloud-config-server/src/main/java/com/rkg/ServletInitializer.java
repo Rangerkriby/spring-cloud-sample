3d06c9adbebeaa20ec99d48fd6f35d91ce7cc638
Rangerkriby <rangerkriby@hotmail.com>
2016-12-19 19:52:22 +0800

    增加配置中心服务
    增加配置中心服务

--- spring-cloud-config-server/src/main/java/com/rkg/ServletInitializer.java
+++ spring-cloud-config-server/src/main/java/com/rkg/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringCloudBizApplication.class);
+		return application.sources(SpringCloudConfigServerApplication.class);
 	}
 
 }
--- spring-cloud-feign/src/main/java/com/rkg/consume/client/ComputeClient.java
+++ spring-cloud-feign/src/main/java/com/rkg/consume/client/ComputeClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(value = "compute-service", fallback = BizClientHystrix.class)
+@FeignClient(value = "biz-service", fallback = BizClientHystrix.class)
 public interface ComputeClient {
 	@RequestMapping(method = RequestMethod.GET, value = "/add")
 	Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
